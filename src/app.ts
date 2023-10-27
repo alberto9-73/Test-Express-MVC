233eac45be2da518f617fbd7816dd24b1822b1d8
alberto9-73 <127358560+alberto9-73@users.noreply.github.com>
2023-10-27 11:24:21 -0300

    INICIO DE LA APP TEST-MVC

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-import  express  from "express";
+import express, { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+const port = 3000;
+
+// configuracion de EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Ruta de home
+app.get('/', (req: Request, res: Response) => {
+
+
+
+    res.render('home',{
+        titulo:'Mi primer noticia',
+        subtitulo:'Probando nueva tecnologia en aplicacion MVC',
+        contenido:'Realizando testing de Express y MVC',
+        TituloDeArchivo:'TEST-MVC',
+        numero:2
+
+});
+})
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando en el puerto:${port}`)
+})
