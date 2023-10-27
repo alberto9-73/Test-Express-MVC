f13d6fb7820c9a29e0905488ce9ce504f08d3b83
alberto9-73 <127358560+alberto9-73@users.noreply.github.com>
2023-10-27 14:57:43 -0300

    hice nuevas pruebas

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,12 @@ app.set('views', path.join(__dirname, 'views'));
 //Ruta de home
 app.get('/', (req: Request, res: Response) => {
 
-
-
     res.render('home',{
         titulo:'Mi primer noticia',
         subtitulo:'Probando nueva tecnologia en aplicacion MVC',
         contenido:'Realizando testing de Express y MVC',
         TituloDeArchivo:'TEST-MVC',
-        numero:2
+        usuario:true
 
 });
 })
